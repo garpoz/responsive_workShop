867b085da29deb597d21bb1b15f2cfa4dc0b5e73
garpoz <garpozir@gmail.com>
2022-06-17 18:52:59 +0200

    edit view file check request auth user pass

--- colleague_django/colleague/views.py
+++ colleague_django/colleague/views.py
@@ -8,16 +8,24 @@ from .models import Hamkar
 import socket, sqlite3
 import requests
 import os
+from django.db.models import Max,Q
 
 
 def index(request):
     if request.GET:
         u_name = request.GET["user"]
         p_name = request.GET["pass"]
-    return render(
-        request,
-        "colleague/index.html",
-        {
-            "headers": "+",
-        },
-    )
+        if p_name.isdigit()==False:
+            return redirect('/')
+        qury=Hamkar.objects.filter(
+            Q(user_name=u_name) & Q(mobile_tel=p_name[1:])
+        )
+        if qury.count()==0:
+            return redirect('/')
+        else:
+            return redirect(f'/market?ok={u_name}')
+    else:
+        return render(
+            request,
+            "colleague/index.html",
+        )
